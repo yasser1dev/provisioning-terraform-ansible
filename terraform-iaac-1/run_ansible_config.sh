c51b76823555ee63028e713acf6fadb0a564b088
Ubuntu <ubuntu@ip-172-31-32-179.us-east-2.compute.internal>
2021-05-19 15:17:47 +0000

    init commit

---
+++ terraform-iaac-1/run_ansible_config.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+mv main.yml ~/self-provisioning/roles/ssl-certificate-generation/vars/
+mv web-site1 ~/static_website_files/
+ansible-playbook ~/self-provisioning/roles/playbook.yaml -i ./inventory.yaml -vvvv
---
+++ terraform-iaac-1/vars.tf
@@ -0,0 +1,12 @@
+variable "default_vpc_id" {
+  type=string
+  default="vpc-21e6704a"
+
+}
+
+variable "ubuntu_ami_id"{
+  type=string
+  default="ami-00399ec92321828f5"
+           
+
+}
